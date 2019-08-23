4be5ea2e20d74fdb4d891ede1027df360adf0332
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-08-23 14:50:49 -0500

    Add ReplyBuilderImplTest
    Added tests for ReplyBuilderImpl.

---
+++ src/test/java/io/github/henryssondaniel/teacup/protocol/ftp/server/ReplyBuilderImplTest.java
@@ -0,0 +1,13 @@
+package io.github.henryssondaniel.teacup.protocol.ftp.server;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ReplyBuilderImplTest {
+  @Test
+  void createImplementation() {
+    assertThat(new ReplyBuilderImpl(1, "message").createImplementation())
+        .isExactlyInstanceOf(ReplyImpl.class);
+  }
+}
