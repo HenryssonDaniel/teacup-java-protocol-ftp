f8555dad7cb43b230978cc7a44e95359a099f4b6
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-11-06 21:22:29 +0100

    Add more tests for Simple

--- src/test/java/io/github/henryssondaniel/teacup/protocol/ftp/server/SimpleTest.java
+++ src/test/java/io/github/henryssondaniel/teacup/protocol/ftp/server/SimpleTest.java
@@ -1,18 +1,13 @@
 package io.github.henryssondaniel.teacup.protocol.ftp.server;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
-import io.github.henryssondaniel.teacup.protocol.Server;
-import java.util.List;
-import java.util.function.Supplier;
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.ftplet.FtpException;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,109 +19,127 @@ class SimpleTest {
   private final Context context = mock(Context.class);
   private final FtpServer ftpServer = mock(FtpServer.class);
   private final Handler handler = mock(Handler.class);
-  private final Object lock = new Object();
   private final Reply reply = mock(Reply.class);
-  private final Reply replyDifferent = mock(Reply.class);
-  private final Server<Context, Request> simpleServer = new Simple(ftpServer, handler);
-  private final Object verifyLock = new Object();
+  private final Simple simple = new Simple(ftpServer, handler);
 
-  @Mock private Supplier<List<Request>> supplierNonExisting;
-  private boolean waitVerify = true;
-  private boolean waiting = true;
+  @Mock private io.github.henryssondaniel.teacup.protocol.server.Handler<Request> handlerRequest;
 
   @BeforeEach
   void beforeEach() {
     MockitoAnnotations.initMocks(this);
 
     when(context.getReply()).thenReturn(reply);
+    when(handler.getReply()).thenReturn(reply);
     when(reply.getMessage()).thenReturn("message");
   }
 
   @Test
-  void removeSupplier() {
-    simpleServer.removeSupplier(supplierNonExisting);
-    verifyZeroInteractions(supplierNonExisting);
-  }
-
-  @Test
-  void setContext() {
-    simpleServer.setContext(context);
+  void createProtocolContext() {
+    simple.createProtocolContext(context, handlerRequest);
 
     verify(context).getReply();
     verifyNoMoreInteractions(context);
 
-    verify(handler).setHandler(any());
+    verifyNoInteractions(ftpServer);
+
+    verify(handler).setHandler(handlerRequest);
     verify(handler).setReply(reply);
     verifyNoMoreInteractions(handler);
 
-    verifyNoInteractions(reply);
+    verifyNoInteractions(handlerRequest);
+  }
+
+  @Test
+  void getKey() {
+    assertThat(simple.getKey(context)).isEqualTo("key");
+
+    verifyNoInteractions(context);
+    verifyNoInteractions(ftpServer);
+    verifyNoInteractions(handler);
+  }
+
+  @Test
+  void isEquals() {
+    assertThat(simple.isEquals(context, handler)).isTrue();
+
+    verify(context).getReply();
+    verifyNoMoreInteractions(context);
+
+    verifyNoInteractions(ftpServer);
+
+    verify(handler).getReply();
+    verifyNoMoreInteractions(handler);
+  }
+
+  @Test
+  void isEqualsWhenNotEqualCode() {
+    when(reply.getCode()).thenReturn(1, 0);
+
+    assertThat(simple.isEquals(context, handler)).isFalse();
+
+    verify(context).getReply();
+    verifyNoMoreInteractions(context);
+
+    verifyNoInteractions(ftpServer);
+
+    verify(handler).getReply();
+    verifyNoMoreInteractions(handler);
+  }
+
+  @Test
+  void isEqualsWhenNotEqualMessage() {
+    when(reply.getMessage()).thenReturn("1", "0");
+
+    assertThat(simple.isEquals(context, handler)).isFalse();
+
+    verify(context).getReply();
+    verifyNoMoreInteractions(context);
+
+    verifyNoInteractions(ftpServer);
+
+    verify(handler).getReply();
+    verifyNoMoreInteractions(handler);
+  }
+
+  @Test
+  void serverCleanup() {
+    simple.serverCleanup(handler);
+
+    verifyNoInteractions(ftpServer);
+
+    verify(handler).setReply(null);
+    verifyNoMoreInteractions(handler);
   }
 
   @Test
   void setUp() throws FtpException {
-    simpleServer.setUp();
+    simple.setUp();
+
     verify(ftpServer).start();
     verifyNoMoreInteractions(ftpServer);
+
+    verifyNoInteractions(handler);
   }
 
   @Test
   void setUpWhenException() throws FtpException {
     doThrow(new FtpException("test")).when(ftpServer).start();
 
-    simpleServer.setUp();
+    simple.setUp();
 
     verify(ftpServer).start();
     verifyNoMoreInteractions(ftpServer);
+
+    verifyNoInteractions(handler);
   }
 
   @Test
   void tearDown() {
-    simpleServer.tearDown();
+    simple.tearDown();
+
     verify(ftpServer).stop();
     verifyNoMoreInteractions(ftpServer);
-  }
 
-  private Thread createThread(int code) {
-    var thread =
-        new Thread(
-            () -> {
-              doAnswer(invocationOnMock -> waiting(code)).when(replyDifferent).getCode();
-              setSecondContext();
-            });
-    thread.start();
-    return thread;
-  }
-
-  private void interrupt(Thread thread) throws InterruptedException {
-    synchronized (lock) {
-      while (waiting) lock.wait(1L);
-
-      thread.interrupt();
-    }
-  }
-
-  private void removeSupplier(Supplier<List<Request>> supplier) throws InterruptedException {
-    synchronized (lock) {
-      while (waiting) lock.wait(1L);
-
-      simpleServer.removeSupplier(supplier);
-    }
-  }
-
-  private void setSecondContext() {
-    synchronized (verifyLock) {
-      simpleServer.setContext(context);
-      waitVerify = false;
-      verifyLock.notifyAll();
-    }
-  }
-
-  private Object waiting(Object object) {
-    synchronized (lock) {
-      waiting = false;
-      lock.notifyAll();
-    }
-
-    return object;
+    verifyNoInteractions(handler);
   }
 }
